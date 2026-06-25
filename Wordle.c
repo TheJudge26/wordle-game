5c0cd7a56cbd307858910be4781f0bab8087b62a
Danial Awny <danialawny26@gmail.com>
2026-06-26 02:01:52 +0300

    fixed bugs

--- Wordle.c
+++ Wordle.c
@@ -16,28 +16,94 @@
 typedef char Result;
 typedef Result Results[5];
 
+void Example_print_results(Results);
+void cw(char*, char*, Results);
 Result cc(char, int, char*);
-int main();
+int isin(char target_letter, char *word);
+int main(int, char**);
 
+
+void Example_print_results(Results res){
+    int i;
+
+    for ( i = 0; i < 5; i++)
+    {
+        switch (res[i])
+        {
+        case ResultGreen:
+            printf("%s\n", "Green");
+            break;
+        case ResultYellow:
+
+            printf("%s\n", "Yellow");
+            break;
+
+        case ResultRed:
+            printf("%s\n", "Red");
+            break;
+        default:
+
+            printf("UNknow %d\n", res[i]);
+            break;
+        }
+    }
+    
+    return;
+}
 Result cc(char guess, int idx, char *word){
     char correct;
     correct=word[idx];
 
-    switch (word[idx])
+    if (guess==correct)
     {
-    case correct:
         return ResultGreen;
-        break;
-    
-    default:
-        if (isin(correct, word))
-            return ResultYellow;
-        break;
     }
-    return ResultRed;
+    else if (isin(guess, word))
+    {
+        return ResultYellow;
+    }
+    else{
+        return ResultRed;
+    }
+    
 }
 
-int main() {
-    printf("\nabc\n");
+
+int isin(char target_letter, char *word) {
+    for (int i = 0; i < 5; i++) {
+        if (word[i] == target_letter) {
+            return 1; 
+        }
+    }
+    return 0; 
+}
+
+
+void cw(char *guess, char *word, Results out_res) {
+   
+    for (int i = 0; i < 5; i++) {
+        
+        out_res[i] = cc(guess[i], i, word);
+    }
+}
+int main(int argc, char *argv[]) {
+    char *correct, *guess;
+    Results res;
+
+    if (argc<3)
+    {
+        fprintf(stderr,"use %s corect guess",argv[0]);
+        return -1;
+    }
+
+    correct = argv[1];
+    guess = argv[2];
+
+    cw(guess, correct, res);
+    // res=cw(correct,guess);
+
+    Example_print_results(res);
+
+    
     return 0;
 }
