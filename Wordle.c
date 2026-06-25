e1cfcd3315fc061074f4ff9ff6ddf6e346f48548
Danial Awny <danialawny26@gmail.com>
2026-06-25 19:01:33 +0300

    check

--- Wordle.c
+++ Wordle.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
-                   // need to make my own (unistd.h)
+// need to make my own (unistd.h)
 #include <stdlib.h>
 #include <string.h>
 
+#define ResultGreen 1
+#define ResultYellow 2
+#define ResultRed 4
 
+// typedef struct{
+//     char color[5];
+
+
+// }Result;
+
+typedef char Result;
+typedef Result Results[5];
+
+Result cc(char, int, char*);
+int main();
+
+Result cc(char guess, int idx, char *word){
+    char correct;
+    correct=word[idx];
+
+    switch (word[idx])
+    {
+    case correct:
+        return ResultGreen;
+        break;
+    
+    default:
+        if (isin(correct, word))
+            return ResultYellow;
+        break;
+    }
+    return ResultRed;
+}
+
+int main() {
+    printf("\nabc\n");
+    return 0;
+}
